3e75a7611975195daf37f143dd842c4ecb9d078a
Deon Wu <148633883+cheerspanda@users.noreply.github.com>
2024-10-28 17:19:59 +0800

    edit version openzeppeline for erc 1155

--- src/infomon/InfomonERC1155.sol
+++ src/infomon/InfomonERC1155.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {ERC1155} from "@openzeppelin/contracts@4.8.2/token/ERC1155/ERC1155.sol";
-import {AccessControl} from "@openzeppelin/contracts@4.8.2/access/AccessControl.sol";
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract InfomonERC1155 is ERC1155, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
